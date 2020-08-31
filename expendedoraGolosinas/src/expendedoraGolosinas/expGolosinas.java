c7d40e55b01136cde8fe4b45be3b0b26ebe4f729
julio cesar del rosario michel <juliocdelrmic@hotmail.com>
2020-08-31 16:08:37 -0700

    terminamos de nombrar las golosinas y les damos precio

--- expendedoraGolosinas/src/expendedoraGolosinas/expGolosinas.java
+++ expendedoraGolosinas/src/expendedoraGolosinas/expGolosinas.java
@@ -9,9 +9,7 @@ public class expGolosinas {
 	public static void main(String[] args) {
 		//llamamos al menu
 		menu();
-		
-		
-		
+				
 	}
 	/*
 	 * muestra el menu, solicita la opcion
@@ -22,8 +20,18 @@ public class expGolosinas {
 		String[][] nombreGolosinas = {
 				{"Kitkat", "Chicles de fresa", "Pulparindos", "Paletas"},
 				{"Kinder Bueno", "Bolsa de dulces variado", "Cheetos", "Snicker"},
-				{""},
+				{"M&M´s", "Papas Sabritas", "Chicles de menta", "Gomitas"},
+				{"Yakult", "Crunch", "Milkybar", "Duvalin"},
+				
 		};
+		//precio de las golosinas
+		double [][] precio = {
+				{1.1, 0.8, 1.5, 0.9},
+				{1.8, 1, 1.2, 1},
+				{1.8, 1.3, 1.2, 0.8},
+				{1.5, 1.1, 1.1, 1.1}
+		};
+		
 		
 	}
 
