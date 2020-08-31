647922a34917d09848b7d2102c7c50735c6fdebd
julio cesar del rosario michel <juliocdelrmic@hotmail.com>
2020-08-31 16:40:12 -0700

    rellenamos matriz con 5 e indicamos si deseamos salir o no, creamos bucle con opciones, implementamos un bloque try-catch.

--- expendedoraGolosinas/src/expendedoraGolosinas/expGolosinas.java
+++ expendedoraGolosinas/src/expendedoraGolosinas/expGolosinas.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class expGolosinas {
 	//creamos un scanner estatico
-	Scanner sc = new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
 		//llamamos al menu
@@ -31,8 +31,39 @@ public class expGolosinas {
 				{1.8, 1.3, 1.2, 0.8},
 				{1.5, 1.1, 1.1, 1.1}
 		};
+		int cantidad[][] = new int[4][4];
+		//rellenar matriz con 5
+		rellenarMatriz(cantidad, 5);
+		//indicamos si salimos o no
+		boolean salir = false;
+		int opcion, fila, columna, cantidadNueva;
+		String pos, password;
+		double ventasTotales = 0;
 		
-		
+		//bucle para pedir las opciones hasta que se elija salir
+		while(!salir) {
+			//opciones
+			System.out.println("1. Pedir golosinas");
+			System.out.println("2. Mostrar golosinas");
+			System.out.println("3. Rellenar golosinas");
+			System.out.println("4. Apagar maquina");
+			
+			try {
+				//pide opcion
+				System.out.println("Introduce numero");
+				opcion = sc.nextInt();
+				
+				//realiza una de las opciones
+				switch (opcion) {
+				case 1:
+					//pido la posicion
+					pos = pedirCadena("Introdusca la posicion que deseada");
+					
+					//validamos la posicion
+					
+				}
+			}
+		}
 	}
 
 }
