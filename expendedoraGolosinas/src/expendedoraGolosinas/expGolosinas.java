b33ae25cf9dc37898318a6bcbadcb740ba38949f
julio cesar del rosario michel <juliocdelrmic@hotmail.com>
2020-08-31 15:29:23 -0700

    creamos un scaner estatico y un menu

---
+++ expendedoraGolosinas/src/expendedoraGolosinas/expGolosinas.java
@@ -0,0 +1,29 @@
+package expendedoraGolosinas;
+
+import java.util.Scanner;
+
+public class expGolosinas {
+	//creamos un scanner estatico
+	Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		//llamamos al menu
+		menu();
+		
+		
+		
+	}
+	/*
+	 * muestra el menu, solicita la opcion
+	 * y la ejecuta
+	 * */
+	private static void menu() {
+		//nombramos las golosinas
+		String[][] nombreGolosinas = {
+				{"Kitkat", "Chicles de fresa", "Pulparindos", "Paletas"},
+				
+		};
+		
+	}
+
+}
