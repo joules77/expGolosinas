b4f88a0809239ac06455382bf908218497d7c1fe
julio cesar del rosario michel <juliocdelrmic@hotmail.com>
2020-09-04 16:00:00 -0700

    aqui finalizamos nuestro bloque try-catch, iniciamos con la creacion de las clases de nuestros metodos.

--- expendedoraGolosinas/src/expendedoraGolosinas/expGolosinas.java
+++ expendedoraGolosinas/src/expendedoraGolosinas/expGolosinas.java
@@ -1,5 +1,6 @@
 package expendedoraGolosinas;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class expGolosinas {
@@ -60,10 +61,68 @@ public class expGolosinas {
 					pos = pedirCadena("Introdusca la posicion que deseada");
 					
 					//validamos la posicion
+					if(validarPos(nombreGolosinas, pos)) {
+						//extraigo la fila y la columna
+						fila = extraerNumero(pos, 0);
+						columna = extraerNumero(pos, 1);
+						//indico si hay valores en la matriz
+						if(hayValorPosicion(cantidad, fila, columna)) {
+							//muestro golosina
+							System.out.println("Aqui tiene su golosina: " +nombreGolosinas[fila][columna] +"Gracias por su compra");
+							//reduzco la cantidad en 1
+							reducirPosicion(cantidad, fila, columna, 1);
+							//aumento la cantidad
+							ventasTotales += precio[fila][columna];
+						}else {
+							System.out.println("No hay mas golosinas de este tipo, elija otra opcion");
+						}
+					}else {
+						System.out.println("La posicion introducida no es valida intente de nuevo");
+					}
+					break;
+				case 2:
+					//muestro las golosinas
+					mostrarGolosinas(nombreGolosinas, precio);
+					break;
+				case 3:
+					//compruebo si es correcta la contraseña
+					if(cadenaIguales(password, "Maquina Expendedora 2020")) {
+						//pido la posicion
+						pos = pedirCadena("Introduce la posicion que quiera");
+						//extraigo los numeros
+						fila = extraerNumero(pos, 0);
+						columna = extraerNumero(pos, 1);
+						//validamos la posicion dada
+						if(validarPos(nombreGolosinas, pos)) {
+							//introducimos la cantidad
+							cantidadNueva = pedirInteger("Introduce la cantidad");
+							//aumentamos la cantidad en el valor dado
+							aumentarPosicion(cantidad, fila, columna, cantidadNueva);
+							System.out.println("Se ha incrementado la cantidad");
+						}else {
+							System.out.println("La posicion introducida no es valida");
+						}
+						
+					}else {
+						System.out.println("La contraseña es incorrecta");
+					}
+					break;
+				case 4:
+					//muestro las ventas
+					System.out.println("Las ventas han sido de: " + ventasTotales);
 					
+					salir = true;
+					break;
+					default:
+						System.out.println("las ocupaciones son entre 1 y 7");
 				}
+			//controla la excepcion en caso de que se introduzca un valor incorrecto	
+			}catch(InputMismatchException e) {
+				System.out.println("Debes ecribir un numero");
+				sc.next();
 			}
 		}
+		System.out.println("Fin del menu");
 	}
 
 }
