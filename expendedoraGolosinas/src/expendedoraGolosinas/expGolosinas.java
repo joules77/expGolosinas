cc85647f160831e3f22920d0381871721453dea0
julio cesar del rosario michel <juliocdelrmic@hotmail.com>
2020-09-12 14:55:46 -0700

    terminacion de programa

--- expendedoraGolosinas/src/expendedoraGolosinas/expGolosinas.java
+++ expendedoraGolosinas/src/expendedoraGolosinas/expGolosinas.java
@@ -85,6 +85,8 @@ public class expGolosinas {
 					mostrarGolosinas(nombreGolosinas, precio);
 					break;
 				case 3:
+					//pido el password
+					password = pedirCadena("Intriduce la contraseña");
 					//compruebo si es correcta la contraseña
 					if(cadenaIguales(password, "Maquina Expendedora 2020")) {
 						//pido la posicion
@@ -123,6 +125,145 @@ public class expGolosinas {
 			}
 		}
 		System.out.println("Fin del menu");
+	}
+	/*
+	 * rellena la matriz con numeros
+	 * param matriz
+	 * param num
+	 * */
+	public static void rellenarMatriz(int[][] matriz, int num) {
+		for(int i = 0; i < matriz.length; i++) {
+			for(int j = 0; j < matriz.length; j++) {
+				matriz[i][j] = num;
+			}
+		}
+	}
+	/*
+	 * pide una cadena
+	 * param mensaje
+	 * return
+	 * */
+	public static String pedirCadena(String mensaje) {
+		System.out.println(mensaje);
+		String cadena = sc.next();
+		
+		return cadena;
+	}
+	/*
+	 * pedir un integer
+	 * param mensaje
+	 * return
+	 * */
+	public static int pedirInteger(String mensaje) {
+		System.out.println(mensaje);
+		int numero = sc.nextInt();
+		
+		return numero;
+	}
+	/*
+	 * valida si una posicion es correcta
+	 * param matriz
+	 * param pos
+	 * return
+	 * */
+	public static boolean validarPos(String [][] matriz, String pos) {
+		if(pos.length() != 2) {
+			return false;
+		}
+		if(!(esNumero(pos.substring(0, 1)) && esNumero(pos.substring(1, 2)))) {
+			return false;
+		}
+		int fila = extraerNumero(pos, 0);
+		int columna = extraerNumero(pos, 1);
+		
+		if(!((fila >= 0 && fila < matriz.length) && (columna >= 0 && columna < matriz[0].length))) {
+			return false;
+		}
+		return true;
+	}
+	/*
+	 * indico si un string es un numero
+	 * param num
+	 * return
+	 * */
+	public static boolean esNumero(String num) {
+		try {
+			Integer.parseInt(num);
+			return true;
+		}catch(NumberFormatException e) {
+			return false;
+		}
+	}
+	/*
+	 * extraigo el numero, -1 si ha habido un error
+	 * param numero
+	 * param pos
+	 * return
+	 * */
+	public static int extraerNumero(String numero, int pos) {
+		int num = -1;
+		if(esNumero(numero)) {
+			num = Integer.parseInt(numero.substring(pos, pos + 1));
+		}
+		return num;
+	}
+	/*
+	 * reduce la cantidad de una posicion en concreto
+	 * param matriz
+	 * param fila
+	 * param columna
+	 * param cantidad
+	 * */
+	public static void reducirPosicion(int[][] matriz, int fila, int columna, int cantidad) {
+		matriz[fila][columna] -= cantidad;
+	}
+	/*
+	 * indica si en una posicion hay un valor mayor que 0
+	 * param matriz
+	 * param fila
+	 * param columna
+	 * return
+	 * */
+	public static boolean hayValorPosicion(int[][] matriz, int fila, int columna) {
+		if(matriz[fila][columna] > 0) {
+			return true;
+		}
+		return false;
+	}
+	/*
+	 * muestra las golosinas
+	 * param nombres
+	 * param precios
+	 * */
+	public static void mostrarGolosinas(String[][] nombres, double[][] precios) {
+		for(int i = 0; i < nombres.length; i++) {
+			for(int j = 0; j < nombres[i].length; j++) {
+				System.out.println(i + "" + j + "" + nombres[i][j] + "" + precios[i][j]);
+			}
+		}
+	}
+	/*
+	 * indica si dos cadenas son iguales
+	 * param cadena1
+	 * param cadena2
+	 * return
+	 * */
+	public static boolean cadenaIguales(String cadena1, String cadena2) {
+		if(cadena1.equals(cadena2)) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	/*
+	 * aumenta la cantidad de una posicion
+	 * param matriz
+	 * param fila
+	 * param columna
+	 * param cantidad
+	 * */
+	public static void aumentarPosicion(int[][] matriz, int fila, int columna, int cantidad) {
+		matriz[fila][columna] += cantidad;
 	}
 
 }
